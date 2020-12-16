16356a13ea7d6b173d989fa2262db1038716fd29
Rachel Reilly <r.a.reill18@gmail.com>
2020-12-16 11:41:50 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project was created following along with the Node CLI tutorial [here](https://www.twilio.com/blog/how-to-build-a-cli-with-node-js)
---
+++ src/main.js
@@ -0,0 +1,82 @@
+import chalk from 'chalk';
+import execa from 'execa';
+import fs from 'fs';
+import Listr from 'listr';
+import ncp from 'ncp';
+import path from 'path';
+import { projectInstall } from 'pkg-install';
+import { promisify } from 'util';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options) {
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize a git repository'))
+  }
+  return;
+}
+
+export async function createProject(options) {
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || process.cwd(),
+  };
+
+  const currentFileUrl = import.meta.url;
+  const templateDir = path.resolve(
+    new URL(currentFileUrl).pathname,
+    '../../templates',
+    options.template.toLowerCase()
+  );
+  options.templateDirectory = templateDir;
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+    process.exit(1);
+  }
+
+  console.log('Copy project files');
+  await copyTemplateFiles(options);
+
+  console.log('%s Project ready', chalk.green.bold('DONE'));
+
+  const tasks = new Listr([
+    {
+      title: 'Copy project files',
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: 'Initalize git',
+      task: () => initGit(options),
+      enabled: () => options.git
+    },
+    {
+      title: 'Install dependencies',
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory
+        }),
+      skip: () =>
+        !options.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined
+    }
+  ]);
+
+  await tasks.run();
+  console.log('%s Project ready', chalk.green.bold('DONE'));
+
+  return true;
+}
